cc03bc1f08f15df91abf4ef5a0ea72fee6730f96
Artem Russkikh <ra@itbeaver.co>
2015-12-02 05:09:36 +0500

    Add monarch_name to Monarchy

--- models/monarchy/Monarchy.cs
+++ models/monarchy/Monarchy.cs
@@ -3,12 +3,25 @@ using System;
 
 class Monarchy:State
 {
+  protected string monarch_name;
+
   public Monarchy() {
     this.id = State.count;
     this.name = "Гоcударство (монархия) " + id;
+    this.monarch_name = "Вася III";
+  }
+
+  public Monarchy(string name, string monarch_name) {
+    this.name = name;
+    this.monarch_name = monarch_name;
   }
 
   public override void Show() {
-    Console.WriteLine("Монархия.\n ID: {0}\n Название: {1} \n", id, name);
+    Console.WriteLine("Монархия.\n ID: {0}\n Название: {1} \n Имя монарха: {2} \n", id, name, monarch_name);
+  }
+
+  public string MonarchName {
+    get { return monarch_name; }
+    set { monarch_name=value; }
   }
 }
